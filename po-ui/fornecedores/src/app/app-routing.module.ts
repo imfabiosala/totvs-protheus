8c6639b94a76142c79f21d0dbdeb736552d5f007
Fábio Sala <imfabiosala@gmail.com>
2023-09-09 19:54:40 -0300

    config. das rotas.

--- po-ui/fornecedores/src/app/app-routing.module.ts
+++ po-ui/fornecedores/src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListarFornecedoresComponent } from './pages/listar/listar-fornecedores.component';
+import { CadastrarFornecedorComponent } from './pages/cadastrar/cadastrar-fornecedor.component';
+import { VisualizarFornecedorComponent } from './pages/visualizar/visualizar-fornecedor.component';
+import { AlterarFornecedorComponent } from './pages/alterar/alterar-fornecedor.component';
+import { ExcluirFornecedorComponent } from './pages/excluir/excluir-fornecedor.component';
+
+const routes: Routes = [
+  {
+    path: 'fornecedores',
+    component: ListarFornecedoresComponent
+  },
+  {
+    path: 'fornecedores/cadastrar',
+    component: CadastrarFornecedorComponent
+  },
+  {
+    path: 'fornecedores/visualizar/:codigo',
+    component: VisualizarFornecedorComponent
+  },
+  {
+    path: 'fornecedores/alterar/:codigo',
+    component: AlterarFornecedorComponent
+  },
+  {
+    path: 'fornecedores/excluir/:codigo',
+    component: ExcluirFornecedorComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'fornecedores'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
